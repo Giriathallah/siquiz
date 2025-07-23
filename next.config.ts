3e34125da266ac7693b48e7a90894fbcaddc6e4a
Giriathallah <athallahgiri@gmail.com>
2025-07-24 01:57:44 +0700

    add output di next config dan pjson

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   // Opsi ini akan mengabaikan error ESLint
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
   },
 };
 
